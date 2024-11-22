242453a4ead3df5d3dca24108c5228d7ae94101b
DavidDing21 <dinghonghe0307@gmail.com>
2024-11-22 20:28:12 +0800

    Initialization

---
+++ run.py
@@ -0,0 +1,111 @@
+import argparse
+import os
+import torch
+import random
+import numpy as np
+from exp.exp_main import Exp_Main
+
+parser = argparse.ArgumentParser(description='CycleNet for Time Series Forecasting')
+
+# random seed
+parser.add_argument('--random_seed', type=int, default=1024, help='random seed')
+
+# basic config
+parser.add_argument('--is_training', type=int, required=True, default=1, help='status')
+parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
+parser.add_argument('--model', type=str, required=True, default='CycleNet', help='model name')
+
+# data loader
+parser.add_argument('--data', type=str, required=True, default='electricity', help='dataset type')
+parser.add_argument('--root_path', type=str, default='./data/', help='root path of the data file')
+parser.add_argument('--data_path', type=str, default='electricity.csv', help='data file')
+parser.add_argument('--features', type=str, default='M', help='forecasting task, options:[M, S, MS]')
+parser.add_argument('--target', type=str, default='OT', help='target feature')
+parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+
+# forecasting task
+parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+parser.add_argument('--label_len', type=int, default=0, help='start token length')
+parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+
+# CycleNet specific
+parser.add_argument('--cycle', type=int, default=168, help='cycle length')
+parser.add_argument('--model_type', type=str, default='mlp', help='model type, options: [linear, mlp]')
+parser.add_argument('--use_revin', type=int, default=1, help='1: use revin or 0: no revin')
+parser.add_argument('--enc_in', type=int, default=321, help='encoder input size')
+parser.add_argument('--dec_in', type=int, default=321, help='decoder input size')
+parser.add_argument('--c_out', type=int, default=321, help='output size')
+
+# optimization
+parser.add_argument('--num_workers', type=int, default=4, help='data loader num workers')
+parser.add_argument('--train_epochs', type=int, default=30, help='train epochs')
+parser.add_argument('--batch_size', type=int, default=128, help='batch size of train input data')
+parser.add_argument('--patience', type=int, default=5, help='early stopping patience')
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+parser.add_argument('--loss', type=str, default='mse', help='loss function')
+parser.add_argument('--lradj', type=str, default='type3', help='adjust learning rate')
+parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+
+# GPU
+parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+parser.add_argument('--gpu', type=int, default=0, help='gpu')
+parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+parser.add_argument('--devices', type=str, default='0,1', help='device ids of multile gpus')
+
+args = parser.parse_args()
+
+# Set random seed
+fix_seed = args.random_seed
+random.seed(fix_seed)
+torch.manual_seed(fix_seed)
+np.random.seed(fix_seed)
+
+args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
+
+if args.use_gpu and args.use_multi_gpu:
+    args.devices = args.devices.replace(' ', '')
+    device_ids = args.devices.split(',')
+    args.device_ids = [int(id_) for id_ in device_ids]
+    args.gpu = args.device_ids[0]
+
+print('Args in experiment:')
+print(args)
+
+Exp = Exp_Main
+
+if args.is_training:
+    setting = '{}_{}_{}_ft{}_sl{}_pl{}_cycle{}_{}_seed{}'.format(
+        args.model_id,
+        args.model,
+        args.data,
+        args.features,
+        args.seq_len,
+        args.pred_len,
+        args.cycle,
+        args.model_type,
+        fix_seed)
+
+    exp = Exp(args)
+    print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+    exp.train(setting)
+
+    print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+    exp.test(setting)
+    
+    torch.cuda.empty_cache()
+else:
+    setting = '{}_{}_{}_ft{}_sl{}_pl{}_cycle{}_{}_seed{}'.format(
+        args.model_id,
+        args.model,
+        args.data,
+        args.features,
+        args.seq_len,
+        args.pred_len,
+        args.cycle,
+        args.model_type,
+        fix_seed)
+
+    exp = Exp(args)
+    print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+    exp.test(setting, test=1)
+    torch.cuda.empty_cache()

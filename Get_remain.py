94f2ce83ad054ee4db18a5c48635a714025dfc4a
ccmkccmk <22096793d@connect.polyu.hk>
2024-11-26 23:33:16 +0800

    Refactor Get_remain.py to include data processing, model loading, and saving results for electricity dataset

--- Get_remain.py
+++ Get_remain.py
@@ -1,80 +1,148 @@
 import os
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+import torch
+from models import CycleNet
 from utils.timefeatures import time_features
 
 scale = True
-url = r''
+url = r'dataset/electricity.csv'
+setting = 'Electricity_720_720_CycleNet_custom_ftM_sl720_pl720_cycle168_linear_seed1024'
+_seq_len = 720
+_set_type = 2 # 0: train, 1: vali, 2: test
+_cycle = 168
+_label_len = 0
+_pred_len = 720
+index = 0
+flag = 'all'
+_scaler = StandardScaler()
+df_raw = pd.read_csv(url)
+_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def __read_data__(self):
-    _scaler = StandardScaler()
-    df_raw = pd.read_csv(url)
+'''
+df_raw.columns: ['date', ...(other features), target feature]
+'''
+cols = list(df_raw.columns)
+cols.remove('OT')
+cols.remove('date')
+df_raw = df_raw[['date'] + cols + ['OT']] # move the target feature to the last column
 
-    '''
-    df_raw.columns: ['date', ...(other features), target feature]
-    '''
-    cols = list(df_raw.columns)
-    cols.remove('OT')
-    cols.remove('date')
-    df_raw = df_raw[['date'] + cols + ['OT']]
-    # print(cols)
-    num_train = int(len(df_raw) * 0.7)
-    num_test = int(len(df_raw) * 0.2)
-    num_vali = len(df_raw) - num_train - num_test
-    border1s = [0, num_train - _seq_len, len(df_raw) - num_test - _seq_len]
-    border2s = [num_train, num_train + num_vali, len(df_raw)]
-    border1 = border1s[_set_type]
-    border2 = border2s[_set_type]
+print(df_raw.shape)
+num_train = int(len(df_raw) * 0.7) # 70% of the data is used for training
+num_test = int(len(df_raw) * 0.2) # 20% of the data is used for testing
+num_vali = len(df_raw) - num_train - num_test # 10% of the data is used for validation
+border1s = [0, num_train - _seq_len, len(df_raw) - num_test - _seq_len] # 0, 70% of the data - seq_len, 80% of the data - seq_len
+border2s = [num_train, num_train + num_vali, len(df_raw)] # 70% of the data, 80% of the data, 100% of the data
+border1 = border1s[_set_type] # in test case, border1 = 80% of the data - seq_len
+border2 = border2s[_set_type] # in test case, border2 = 100% of the data
 
-    cols_data = df_raw.columns[1:]
-    df_data = df_raw[cols_data]
+cols_data = df_raw.columns[1:] # all columns except the date column
+df_data = df_raw[cols_data] # all columns except the date column
 
-    if scale:
-        train_data = df_data[border1s[0]:border2s[0]]
-        _scaler.fit(train_data.values)
-        # print(_scaler.mean_)
-        # exit()
-        data = _scaler.transform(df_data.values)
-    else:
-        data = df_data.values
+# if scale:
+train_data = df_data[border1s[0]:border2s[0]] 
+_scaler.fit(train_data.values) # fit the scaler to the training data
+# print(_scaler.mean_)
+# exit()
+data = _scaler.transform(df_data.values)[:-1] # scale the data
+print(data.shape)
+# else:
+#     data = df_data.values
 
-    df_stamp = df_raw[['date']][border1:border2]
-    df_stamp['date'] = pd.to_datetime(df_stamp.date)
-    if _timeenc == 0:
-        df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
-        df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-        df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
-        df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
-        data_stamp = df_stamp.drop(['date'], 1).values
-    elif _timeenc == 1:
-        data_stamp = time_features(pd.to_datetime(df_stamp['date'].values))
-        data_stamp = data_stamp.transpose(1, 0)
+df_stamp = df_raw[['date']] # only the date column
+df_stamp['date'] = pd.to_datetime(df_stamp.date) # convert the date column to datetime
+data_stamp = time_features(pd.to_datetime(df_stamp['date'].values)) # get the time features
+data_stamp = data_stamp.transpose(1, 0) # transpose the time features to match the shape of the data
+_data_x = data # all data
+_data_y = data # all data
+_data_stamp = data_stamp # time features of all test data
 
-    _data_x = data[border1:border2]
-    _data_y = data[border1:border2]
-    _data_stamp = data_stamp
+def inverse_transform(data):
+    return _scaler.inverse_transform(data) # inverse the scaling
 
-    # add cycle
-    _cycle_index = (np.arange(len(data)) % _cycle)[border1:border2]
+class Configs:
+    def __init__(self, seq_len, pred_len, enc_in, cycle, model_type, d_model, use_revin):
+        self.seq_len = seq_len
+        self.pred_len = pred_len
+        self.enc_in = enc_in
+        self.cycle = cycle
+        self.model_type = model_type
+        self.d_model = d_model
+        self.use_revin = use_revin
 
-def __getitem__(self, index):
-    s_begin = index
-    s_end = s_begin + _seq_len
-    r_begin = s_end - _label_len
-    r_end = r_begin + _label_len + _pred_len
+configs = Configs(
+    seq_len=_seq_len,
+    pred_len=_pred_len,
+    enc_in=320,
+    cycle=_cycle,
+    model_type='linear',
+    d_model=512,
+    use_revin=True
+)
 
-    seq_x = _data_x[s_begin:s_end]
-    seq_y = _data_y[r_begin:r_end]
-    seq_x_mark = _data_stamp[s_begin:s_end]
-    seq_y_mark = _data_stamp[r_begin:r_end]
+_model = CycleNet.Model(configs=configs).float()
 
-    cycle_index = torch.tensor(_cycle_index[s_end])
+print('loading model')
+_model.load_state_dict(torch.load(os.path.join('./checkpoints/' + setting, 'checkpoint.pth')))
 
-    return seq_x, seq_y, seq_x_mark, seq_y_mark, cycle_index
+folder_path = './test_results/' + setting + '/'
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
 
-def __len__(self):
-    return len(_data_x) - _seq_len - _pred_len + 1
+# _model.eval()
+# with torch.no_grad():
+#     batch_x = _data_x.float().to(_device)
+#     batch_y = _data_y.float().to(_device)
 
-def inverse_transform(self, data):
-    return _scaler.inverse_transform(data)
+#     batch_cycle = cycle_index.int().to(_device)
+
+#     outputs = _model(batch_x, batch_cycle)
+
+#     f_dim = 0
+#     # print(outputs.shape,batch_y.shape)
+#     outputs = outputs[:, -_args.pred_len:, f_dim:]
+#     batch_y = batch_y[:, -_args.pred_len:, f_dim:].to(_device)
+#     outputs = outputs.detach().cpu().numpy()
+#     batch_y = batch_y.detach().cpu().numpy()
+
+#     pred = outputs  # outputs.detach().cpu().numpy()  # .squeeze()
+#     true = batch_y  # batch_y.detach().cpu().numpy()  # .squeeze()
+
+#     input = batch_x.detach().cpu().numpy()
+#     gt = np.concatenate((input[0, :, -1], true[0, :, -1]), axis=0)
+#     pd = np.concatenate((input[0, :, -1], pred[0, :, -1]), axis=0)
+
+#     # x: (batch_size, seq_len, enc_in), cycle_index: (batch_size,)
+#     print(batch_x.shape, batch_cycle.shape)
+
+Q = _model.cycleQueue.data.detach().cpu().numpy()  # 一个周期的数据
+print(Q.shape)
+# preds_last = pred[0, :, -1].reshape(-1)  # 展平预测结果
+trues_last = _data_y[:,:]#[0, :, -1].reshape(-1)
+# print(trues_last[:,:-1].shape, trues_last[0])
+
+# 计算需要多少个完整周期
+Q_len = Q.shape[0]  # 一个周期的长度
+total_len = len(trues_last)
+num_cycles = (total_len + Q_len - 1) // Q_len  # 向上取整得到需要的周期数
+
+# 将Q重复扩展到足够的长度
+Q_repeated = np.tile(Q, [num_cycles,1])[:total_len]
+
+# 计算remain
+trues_remain = trues_last - Q_repeated
+
+Q_repeated = inverse_transform(Q_repeated,)
+
+trues_remain = inverse_transform(trues_remain)
+
+# 保存remain
+remain = pd.DataFrame(trues_remain)
+remain.to_csv(folder_path + 'remain.csv', index=False)
+
+# 保存Q
+Q = pd.DataFrame(Q)
+Q.to_csv(folder_path + 'Q.csv', index=False)
+print('done')

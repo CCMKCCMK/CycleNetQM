1a4236e0916e2e3e4aaeb586cbb68b6b8595ab79
ccmkccmk <22096793d@connect.polyu.hk>
2024-11-28 00:23:59 +0800

    Update test_MM.sh to support multiple sequence and prediction lengths for the electricity model

--- scripts/CycleNet/Linear-Input-96/test_MM.sh
+++ scripts/CycleNet/Linear-Input-96/test_MM.sh
@@ -5,28 +5,31 @@ data_path_name=electricity.csv
 model_id_name=Electricity
 data_name=electricity
 
-
 model_type='CycleNetMM'
-seq_len=96
-for pred_len in 720
+seq_lens=(96 192 288)
+
+for seq_len in "${seq_lens[@]}"
 do
-for random_seed in 1024
-do
-    python -u run.py \
-      --is_training 0 \
-      --root_path $root_path_name \
-      --data_path $data_path_name \
-      --model_id $model_id_name'_'$seq_len'_'$pred_len \
-      --model $model_name \
-      --data $data_name \
-      --features M \
-      --seq_len $seq_len \
-      --pred_len $pred_len \
-      --enc_in 321 \
-      --cycle 168 \
-      --model_type $model_type \
-      --train_epochs 30 \
-      --patience 5 \
-      --itr 1 --batch_size 64 --learning_rate 0.01 --random_seed $random_seed
-done
+  for pred_len in "${seq_lens[@]}"
+  do
+    for random_seed in 1024
+    do
+      python -u run.py \
+        --is_training 1 \
+        --root_path $root_path_name \
+        --data_path $data_path_name \
+        --model_id ${model_id_name}_${seq_len}_${pred_len} \
+        --model $model_name \
+        --data $data_name \
+        --features M \
+        --seq_len $seq_len \
+        --pred_len $pred_len \
+        --enc_in 321 \
+        --cycle 168 \
+        --model_type $model_type \
+        --train_epochs 30 \
+        --patience 5 \
+        --itr 1 --batch_size 64 --learning_rate 0.01 --random_seed $random_seed
+    done
+  done
 done
